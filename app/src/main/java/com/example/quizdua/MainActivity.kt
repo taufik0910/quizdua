50bf0349ca7ab7413d604134499b3d844d973024
taufik0910 <taufik.cradle@gmail.com>
2022-08-09 04:36:38 +0700

    design main activity

--- app/src/main/java/com/example/quizdua/MainActivity.kt
+++ app/src/main/java/com/example/quizdua/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.quizdua
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
